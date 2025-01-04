406db5cc7ccce144a47740a13c514053f81934b2
Lucas Ha <tuancntt971@gmail.com>
2025-01-05 00:03:16 +0700

    initialize React WordPress starter theme with Vite setup and basic templates

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package WordPress
+ * @version 1.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+<?php get_header(); ?>
+
+<div id="app"></div>
+
+<?php get_footer();
---
+++ search.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package WordPress
+ * @version 1.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+
+<?php
+get_footer();
